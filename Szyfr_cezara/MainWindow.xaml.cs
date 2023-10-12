30a84b9c37b41ada3ad4b362841886f69eefbb41
xbautek <bartosz.ksiazek@microsoft.wsei.edu.pl>
2023-10-12 17:11:12 +0200

    Zadanie done

--- Szyfr_cezara/MainWindow.xaml.cs
+++ Szyfr_cezara/MainWindow.xaml.cs
@@ -25,20 +25,26 @@ namespace Szyfr_cezara
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Szyfruj_Click(object sender, RoutedEventArgs e)
         {
             string szyfr = do_szyfru.Text;
 
-            if (kluczyk.SelectedItem != null && int.TryParse(kluczyk.SelectedItem.ToString(), out int wybranyKlucz))
+            if (kluczyk.SelectedItem != null)
             {
-                // Wywołaj funkcję SzyfrujCezarem z wybranym kluczem
-                zaszyfrowane.Text = SzyfrujCezarem(szyfr, wybranyKlucz);
+                if (int.TryParse(((ComboBoxItem)kluczyk.SelectedItem).Content.ToString(), out int wybranyKlucz))
+                {
+                    zaszyfrowane.Text = SzyfrujCezarem(szyfr, wybranyKlucz);
+                }
+                else
+                {
+                    MessageBox.Show("Nie można przekonwertować wybranego klucza na liczbę całkowitą.");
+                }
             }
             else
             {
                 MessageBox.Show("Musisz w comboboxie coś wybrać!");
             }
-        }//Convert.ToInt32(kluczyk.SelectedValue)
+        }
 
         public string SzyfrujCezarem(string tekst, int klucz)
         {
@@ -47,10 +53,8 @@ namespace Szyfr_cezara
                 return string.Empty;
             }
 
-            // Przygotuj pusty string do przechowania zaszyfrowanego tekstu
             StringBuilder zaszyfrowanyTekst = new StringBuilder();
 
-            // Przesuń każdy znak w tekście o podany klucz
             foreach (char znak in tekst)
             {
                 if (char.IsLetter(znak))
@@ -64,12 +68,62 @@ namespace Szyfr_cezara
                 }
                 else
                 {
-                    // Jeśli znak nie jest literą, zachowaj go bez zmiany
                     zaszyfrowanyTekst.Append(znak);
                 }
             }
 
             return zaszyfrowanyTekst.ToString();
+        }
+
+        public string DeszyfrujCezarem(string zaszyfrowanyTekst, int klucz)
+        {
+            if (string.IsNullOrEmpty(zaszyfrowanyTekst))
+            {
+                return string.Empty;
+            }
+
+            StringBuilder odszyfrowanyTekst = new StringBuilder();
+
+            foreach (char znak in zaszyfrowanyTekst)
+            {
+                if (char.IsLetter(znak))
+                {
+                    char startChar = char.IsUpper(znak) ? 'A' : 'a';
+                    int przesuniecie = (klucz % 26); // zmieniamy znak o przeciwną wartość klucza
+                    odszyfrowanyTekst.Append((char)(((znak - startChar - przesuniecie + 26) % 26) + startChar));
+                }
+                else if (char.IsWhiteSpace(znak))
+                {
+                    continue;
+                }
+                else
+                {
+                    odszyfrowanyTekst.Append(znak);
+                }
+            }
+
+            return odszyfrowanyTekst.ToString();
+        }
+
+        private void Deszyfruj_Click(object sender, RoutedEventArgs e)
+        {
+            string zaszyfrowany = do_odszyfru.Text;
+
+            if (kluczyk_Copy.SelectedItem != null)
+            {
+                if (int.TryParse(((ComboBoxItem)kluczyk_Copy.SelectedItem).Content.ToString(), out int wybranyKlucz))
+                {
+                    odszyfrowane.Text = DeszyfrujCezarem(zaszyfrowany, wybranyKlucz);
+                }
+                else
+                {
+                    MessageBox.Show("Nie można przekonwertować wybranego klucza na liczbę całkowitą.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Musisz w comboboxie coś wybrać!");
+            }
         }
     }
 }
